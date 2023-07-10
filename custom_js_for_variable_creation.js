053285ef60c3c0f5a791f1b10f9a2e883f2e68af
sREY CR <54467701+ShreyCR@users.noreply.github.com>
2023-07-10 12:00:35 +0530

    Add files via upload

---
+++ custom_js_for_variable_creation.js
@@ -0,0 +1,25 @@
+
+
+//     vae e1 = {{Click Element}};            
+
+//     var parent - e1.closest(".woocommerce-loop-product_link");  //finds the closest element to element that got clicked which has got the class woocommerce-loop-product_link
+//     if(parent){                                   //if parent class exists for the element created 
+//     var title = parent.querySelector(".woocommerce-loop-product_title");
+//     var productName = title.textContent;         // class name of the child class inside the parent class of the image     PLESE DO A QUERY SELECTOR WHERE I WANT A CHILD WHICH HAS GOT THIS CLASS    so that it contains the textcontent inside the title
+//     console.log(productName);  
+//     }
+
+
+
+// *********************************** for insertion in Tag manager*****************************************
+
+// function() {                                                   //Only anonymous functions in js files
+//                                                                   // {{Double curly variables to access variables inside JS funcions}}
+//     vae e1 = e.srcElement;                                     //Import click element from built-in-variables > Configure
+
+//     const parent - e1.closest(".woocommerce-loop-product_link");  //finds the closest element to element that got clicked which has got the class woocommerce-loop-product_link
+//     if(parent){                                   //if parent class exists for the element created 
+//     const productName = title.textContent;         // class name of the child class inside the parent class of the image     PLESE DO A QUERY SELECTOR WHERE I WANT A CHILD WHICH HAS GOT THIS CLASS    so that it contains the textcontent inside the title
+//     console.log(productName);  
+//     }
+// }
